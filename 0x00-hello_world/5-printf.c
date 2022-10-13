5aba1e7e24ce9255064d459e5a7f5b7894141546
Divine-Blessing <divine.oviekhaye@gmail.com>
2022-10-13 08:15:54 -0700

    to print out

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  *
  *  * main - Entry point
@@ -8,10 +9,8 @@
  *    * Return: Always 0 (Success)
  *
  *     */
-int main(void)
-{
-		puts("\"Programming is like building a multilingual puzzle");
 
-			return (0);
-
+int main(void) {
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
