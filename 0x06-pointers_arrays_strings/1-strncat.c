85ae840eac11c42dc5decbf8920439b80677222d
Divine-Blessing <divine.oviekhaye@gmail.com>
2022-10-26 12:01:12 -0700

    commit this

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strncat function that concatenates two strings
+ * @src: an input string
+ * @dest: an input string
+ * @n: an input integer
+ * Return: a pointer to the resulting string.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int srclen = 0, i = 0;
+	char *random = dest, *start = src;
+
+	while (*src)
+	{
+		srclen++;
+		src++;
+	}
+
+	while (*dest)
+		dest++;
+
+	if (n > srclen)
+		n = srclen;
+
+	src = start;
+
+	for (; i < n; i++)
+		*dest++ = *src++;
+
+	*dest = '\0';
+	return (random);
+}
